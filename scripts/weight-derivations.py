f5fa4a7fdb92ae0586af605ddcd68fa75f80c655
Lucas Clemente Vella <lvella@gmail.com>
2021-06-06 20:55:36 +0100

    Retrieving D2Q9 weights.

--- scripts/weight-derivations.py
+++ scripts/weight-derivations.py
@@ -3,7 +3,7 @@ import itertools
 
 D = sympy.sympify(2)
 
-C0 = [
+e = [
 	[0, -1,  1,  0,  0,  1, -1,  1, -1],
 	[0,  0,  0, -1,  1,  1, -1, -1,  1]
 ]
@@ -11,35 +11,34 @@ C0 = [
 C = sympy.symbols(f"C:{D}")
 w = sympy.symbols(f"w:{3}")
 w = [w[0], w[1], w[1], w[1], w[1], w[2], w[2], w[2], w[2]]
-
+a = sympy.symbols("a")
 
 orthogonal_tensors = [
 	sympy.sympify(1),
-	2*C[0],
-	2*C[1],
-	2*C[0]**2 - 1,
-	2*C[1]**2 - 1,
-	2*C[0]*C[1]
+	C[0],
+	C[1],
+	C[0]**2 - sympy.sympify(1)/2,
+	C[0]*C[1],
+	C[1]**2 - sympy.sympify(1)/2,
 ]
 
 exp = sympy.exp(-(C[0]**2 + C[1]**2))
 
-def eq(tensor_a, tensor_b):
-	print(tensor_a, tensor_b)
+def eq(tensor):
+	print(tensor)
 	quadrature = 0
 	for i in range(len(w)):
-		sub_list = [(a, b[i]) for a, b in zip(C, C0)]
-		quadrature += w[i] * tensor_a.subs(sub_list) * tensor_b.subs(sub_list)
+		sub_list = [(src, a * dest[i]) for src, dest in zip(C, e)]
+		quadrature += w[i] * tensor.subs(sub_list)**2
 
 	rhs = 1/sympy.pi**(D/2) * sympy.integrate(
-		sympy.integrate(exp*tensor_a*tensor_b, (C[0], -sympy.oo, sympy.oo)),
+		sympy.integrate(exp*tensor**2, (C[0], -sympy.oo, sympy.oo)),
 		(C[1], -sympy.oo, sympy.oo))
 
 	return sympy.Eq(quadrature, rhs)
 
-eqs = [eq(a,b) for a,b in itertools.combinations_with_replacement(orthogonal_tensors, 2)]
-eqs = eqs[:-6]
+eqs = [eq(t) for t in orthogonal_tensors]
 for e in eqs:
 	print(e)
 
-print(sympy.solve(eqs, w))
+print(sympy.solve(eqs, itertools.chain(w, [a])))
