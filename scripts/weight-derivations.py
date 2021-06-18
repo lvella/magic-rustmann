4e174c37c5243e783dbe4e11e0d4333155787824
Lucas Clemente Vella <lvella@gmail.com>
2021-06-18 19:12:55 +0100

    First success of weird format.

--- scripts/weight-derivations.py
+++ scripts/weight-derivations.py
@@ -7,10 +7,10 @@ mpmath.dps = 50
 
 D = sympy.sympify(2)
 e = [
-	[0, -1,  1,  0,  0,  1, -sympy.sympify(3)/2,  1, -1],
-	[0,  0,  0, -1,  1,  1, -sympy.sympify(3)/2, -1,  1]
+	[0, -1,  1,  0,  0,  1, -1, 1, 2, -sympy.sympify(3)/4, -sympy.sympify(9)/8],
+	[0,  0,  0, -1,  1, -1,  1, 1, 2, -sympy.sympify(3)/4, -sympy.sympify(9)/8]
 ]
-w_idx = [0,  1,  2,  1,  2,  3,                   4,  5,  5]
+w_idx = [0,  1,  2,  1,  2,  3,  3, 4, 5,                   6,                   7]
 
 #e = [
 #	[0, -1,  1,  0,  0,  1, -1,  1, -1],
@@ -21,7 +21,7 @@ w_idx = [0,  1,  2,  1,  2,  3,                   4,  5,  5]
 C = sympy.symbols(f"C:{D}")
 
 a = sympy.symbols("a", positive=True)
-unique_w = list(sympy.symbols(f"w:{max(w_idx)+1}", positive=True))
+unique_w = list(sympy.symbols(f"w:{max(w_idx)+1}"))
 w = [unique_w[i] for i in w_idx]
 
 orthogonal_tensors = [
@@ -58,7 +58,7 @@ def least_squares(eqs, symbols):
 	fs = [to_func(eq) for eq in eqs]
 	ff = lambda *xs: [f(*xs) for f in fs]
 
-	x0 = [0.2] * len(symbols)
+	x0 = [1] * len(symbols)
 
 	js = [
 		[to_func(sympy.diff(f, x)) for x in symbols]
@@ -72,6 +72,6 @@ def least_squares(eqs, symbols):
 
 	print(mpmath.findroot(ff, x0, J=jj, solver='mnewton', maxsteps=100000, tol=1e-100, verbose=True))
 
-least_squares(eqs, unique_w + [a])
+#least_squares(eqs, unique_w + [a])
 
 print(sympy.solve(eqs, unique_w + [a]))
